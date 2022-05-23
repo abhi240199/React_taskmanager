01651b3fff88f5befe36bbe19868889bfb748594
Abhishek Patel <ap8209655@gmail.com>
2022-05-23 23:58:04 +0530

    Using react-bootstrap for cart

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      data: [1, 2, 3, 4],
+      data: [1, 2, 3, 4, 5],
       curr_title: "",
       curr_text: "",
     };
@@ -14,11 +14,38 @@ export default class App extends React.Component {
   addItem = () => {};
   render() {
     return (
-      <div>
-        {this.state.data.map((item) => (
-          <Cart />
-        ))}
-      </div>
+      <>
+        <h2>Pending Task</h2>
+        <br></br>
+        <div
+          style={{
+            display: "flex",
+            width: "100%",
+            flexWrap: "wrap",
+            justifyContent: "space-around",
+          }}
+        >
+          {this.state.data.map((item) => (
+            <Cart />
+          ))}
+        </div>
+        <br></br>
+        <h2>Completed Task</h2>
+        <br></br>
+
+        <div
+          style={{
+            display: "flex",
+            width: "100%",
+            flexWrap: "wrap",
+            justifyContent: "space-around",
+          }}
+        >
+          {this.state.data.map((item) => (
+            <Cart />
+          ))}
+        </div>
+      </>
     );
   }
 }
--- src/Cart.js
+++ src/Cart.js
@@ -1,8 +1,28 @@
 // @flow
 import * as React from "react";
+import Card from "react-bootstrap/Card";
+import ListGroup from "react-bootstrap/ListGroup";
+import ListGroupItem from "react-bootstrap/ListGroupItem";
 
 export class Cart extends React.Component {
   render() {
-    return <div>Cart</div>;
+    return (
+      <div>
+        <Card style={{ width: "18rem", marginBottom: 10 }}>
+          <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+          </Card.Body>
+          <ListGroup className="list-group-flush">
+            <ListGroupItem>
+              my name is abhis hdbjhw ,jhdbsf erlsghlg kljeragl my name is abhis
+            </ListGroupItem>
+          </ListGroup>
+          <Card.Body>
+            <Card.Link href="#">Done</Card.Link>
+            <Card.Link href="#">Delete</Card.Link>
+          </Card.Body>
+        </Card>
+      </div>
+    );
   }
 }
