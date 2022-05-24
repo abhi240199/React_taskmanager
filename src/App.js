56b009eef14314b988d882b4a0d5bb4434b93f4f
Abhishek Patel <ap8209655@gmail.com>
2022-05-24 16:40:00 +0530

    added action to task-manager

--- src/App.js
+++ src/App.js
@@ -1,20 +1,116 @@
 // @flow
 import * as React from "react";
 import { Cart } from "./Cart";
+import { CompletedCart } from "./ComletedCart";
 
 export default class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      data: [1, 2, 3, 4, 5],
+      data: [
+        {
+          title: "Study",
+          text: "Daily study",
+          id: 1,
+          completed: false,
+        },
+        {
+          title: "Writing",
+          text: "one page writing",
+          id: 2,
+          completed: false,
+        },
+        {
+          title: "Game",
+          text: "Play Cricket",
+          id: 3,
+          completed: false,
+        },
+      ],
       curr_title: "",
       curr_text: "",
+      id: 4,
     };
   }
-  addItem = () => {};
+  addItem = (e) => {
+    this.setState({
+      id: this.state.id + 1,
+    });
+    var taskArray = this.state.data;
+    taskArray.push({
+      title: this.state.curr_title,
+      text: this.state.curr_text,
+      id: this.state.id,
+      completed: false,
+    });
+    this.setState({
+      data: taskArray,
+    });
+  };
+  handleText = (e) => {
+    this.setState({
+      curr_text: e.target.value,
+    });
+  };
+  handleTitle = (e) => {
+    this.setState({
+      curr_title: e.target.value,
+    });
+  };
+  pendingTask = (id) => {
+    const temp = [...this.state.data];
+    var task = temp.find((item) => item.id === id);
+    task.completed = true;
+    this.setState({
+      data: temp,
+    });
+  };
+  handleDeleteTask = (item) => {
+    const temp = [...this.state.data];
+    const index = temp.indexOf(item);
+    temp.splice(index, 1);
+    this.setState({
+      data: temp,
+    });
+  };
   render() {
     return (
       <>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <div>
+            <h3>Create Task</h3>
+          </div>
+          <br></br>
+
+          <div>
+            <input
+              type="text"
+              placeholder="Add Title"
+              onChange={this.handleTitle}
+            ></input>
+          </div>
+          <div>
+            <textarea
+              type="text"
+              placeholder="About Task"
+              onChange={this.handleText}
+            ></textarea>
+          </div>
+          <div>
+            <input
+              type="submit"
+              placeholder="About Task"
+              onClick={this.addItem}
+            ></input>
+          </div>
+          <br></br>
+        </div>
         <h2>Pending Task</h2>
         <br></br>
         <div
@@ -26,13 +122,17 @@ export default class App extends React.Component {
           }}
         >
           {this.state.data.map((item) => (
-            <Cart />
+            <Cart
+              key={item.id}
+              task={item}
+              pendingTask={this.pendingTask}
+              handleDeleteTask={this.handleDeleteTask}
+            />
           ))}
         </div>
         <br></br>
         <h2>Completed Task</h2>
         <br></br>
-
         <div
           style={{
             display: "flex",
@@ -42,7 +142,11 @@ export default class App extends React.Component {
           }}
         >
           {this.state.data.map((item) => (
-            <Cart />
+            <CompletedCart
+              key={item.id}
+              task={item}
+              handleDeleteTask={this.handleDeleteTask}
+            />
           ))}
         </div>
       </>
--- src/ComletedCart.js
+++ src/ComletedCart.js
@@ -4,12 +4,12 @@ import Card from "react-bootstrap/Card";
 import ListGroup from "react-bootstrap/ListGroup";
 import ListGroupItem from "react-bootstrap/ListGroupItem";
 
-export class Cart extends React.Component {
+export class CompletedCart extends React.Component {
   render() {
-    const { title, text, completed, id } = this.props.task;
+    const { title, text, completed } = this.props.task;
     return (
       <div>
-        {completed === false ? (
+        {completed ? (
           <Card style={{ width: "18rem", marginBottom: 10 }}>
             <Card.Body>
               <Card.Title>{title}</Card.Title>
@@ -18,9 +18,6 @@ export class Cart extends React.Component {
               <ListGroupItem>{text}</ListGroupItem>
             </ListGroup>
             <Card.Body>
-              <Card.Link href="#" onClick={() => this.props.pendingTask(id)}>
-                Done
-              </Card.Link>
               <Card.Link
                 href="#"
                 onClick={() => this.props.handleDeleteTask(this.props.task)}
